5a8e3c3ea4bbcb7260e301e6e3cea347051ba9cc
martuafernando <fernandosibarani45@gmail.com>
2022-09-29 23:12:12 +0700

    add API functions

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'f840b1b9ca749f93dd7bc7320bf2df65',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us'
+}
+
+export default CONFIG
