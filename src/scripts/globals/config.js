6c666792b5d99ec004e4c25cf1a4cf001695773b
martuafernando <fernandosibarani45@gmail.com>
2022-10-03 23:15:42 +0700

    add cache helper

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   KEY: 'f840b1b9ca749f93dd7bc7320bf2df65',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString()
 }
 
 export default CONFIG
